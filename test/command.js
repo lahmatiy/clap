1988cf2e6a71ddc2263078234ec753d8a8376d75
Roman Dvornov <rdvornov@gmail.com>
2019-12-09 01:45:46 +0300

    Fix failure on argv parsing when args & options & literal args are passed

--- test/command.js
+++ test/command.js
@@ -71,6 +71,125 @@ describe('command run', function() {
         });
     });
 
+    describe('args and options', function() {
+        var command;
+        var collectedValues;
+
+        beforeEach(function() {
+            collectedValues = null;
+            command = cli.command('test', '[foo]')
+                .option('--foo', 'Foo')
+                .option('--bar <number>', 'Bar')
+                .action(function(args, literalArgs) {
+                    collectedValues = {
+                        values: this.values,
+                        args,
+                        literalArgs
+                    };
+                });
+        });
+
+        it('no arguments', function() {
+            command.run([]);
+
+            assert.deepEqual(collectedValues, {
+                values: {
+                    foo: false
+                },
+                args: [],
+                literalArgs: []
+            });
+        });
+
+        it('args', function() {
+            command.run(['qux']);
+
+            assert.deepEqual(collectedValues, {
+                values: {
+                    foo: false
+                },
+                args: ['qux'],
+                literalArgs: []
+            });
+        });
+
+        it('options', function() {
+            command.run(['--foo', '--bar', '123']);
+
+            assert.deepEqual(collectedValues, {
+                values: {
+                    foo: true,
+                    bar: 123
+                },
+                args: [],
+                literalArgs: []
+            });
+        });
+
+        it('literal args', function() {
+            command.run(['--', '--one', '--two', '123']);
+
+            assert.deepEqual(collectedValues, {
+                values: {
+                    foo: false
+                },
+                args: [],
+                literalArgs: ['--one', '--two', '123']
+            });
+        });
+
+        it('args & options', function() {
+            command.run(['qux', '--foo', '--bar', '123']);
+
+            assert.deepEqual(collectedValues, {
+                values: {
+                    foo: true,
+                    bar: 123
+                },
+                args: ['qux'],
+                literalArgs: []
+            });
+        });
+
+        it('args & literal args', function() {
+            command.run(['qux', '--', '--one', '--two', '123']);
+
+            assert.deepEqual(collectedValues, {
+                values: {
+                    foo: false
+                },
+                args: ['qux'],
+                literalArgs: ['--one', '--two', '123']
+            });
+        });
+
+        it('options & literal args', function() {
+            command.run(['--foo', '--bar', '123', '--', '--one', '--two', '123']);
+
+            assert.deepEqual(collectedValues, {
+                values: {
+                    foo: true,
+                    bar: 123
+                },
+                args: [],
+                literalArgs: ['--one', '--two', '123']
+            });
+        });
+
+        it('args & options & literal args', function() {
+            command.run(['qux', '--foo', '--bar', '123', '--', '--one', '--two', '123']);
+
+            assert.deepEqual(collectedValues, {
+                values: {
+                    foo: true,
+                    bar: 123
+                },
+                args: ['qux'],
+                literalArgs: ['--one', '--two', '123']
+            });
+        });
+    });
+
     describe('action()', function() {
         it('should have an expected input', function() {
             var calls = [];
